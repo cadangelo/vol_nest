ef19b7d63c55ebca1631a33dd80f2af6ab6dbac7
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2014-03-21 12:23:53 -0500

    Add additional test to sample.

--- sample/sample_app.F90
+++ sample/sample_app.F90
@@ -6,7 +6,6 @@ integer:: result, vol_idx
 character(12):: filename
 real*8:: x, y, z
 
-
 filename = "test.sat"
 
 call dagmcinit(filename,8)
@@ -48,6 +47,18 @@ else
 endif
 
 x=200
+y=0
+z=0
+vol_idx=2 
+
+call dagmcchkcel(x,y,z,vol_idx,result)
+if (result == 0) then
+   write(*,*) "point (",x,",",y,",",z,") is inside volume ",vol_idx
+else
+   write(*,*) "point (",x,",",y,",",z,") is outside volume ",vol_idx
+endif
+
+x=0
 y=0
 z=0
 vol_idx=2 
